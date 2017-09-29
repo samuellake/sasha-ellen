17b653e5b2f2dd3d10dea09848dce4e61d7b3a85
s.lake <s.lake@net-a-porter.com>
2017-09-29 18:13:03 +0100

    lots of temp work getting slick gallery to work

--- public/javascripts/image-gallery.js
+++ public/javascripts/image-gallery.js
@@ -1,37 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var next = document.getElementById("next");
-  var prev = document.getElementById("prev");
+  // var next = document.getElementById("next");
+  // var prev = document.getElementById("prev");
+  //
+  // function showNext(){
+  //   var carousel = document.getElementsByClassName("image-gallery-items");
+  //   var items = carousel[0].children;
+  //   var length = items.length;
+  //   var selected = document.getElementsByClassName("image-gallery-item-selected")[0];
+  //   var index = Array.prototype.indexOf.call(items, selected);
+  //   var next = index >= length - 1 ? 0 : index + 1;
+  //
+  //   console.log(items[index], items[next]);
+  //
+  //
+  //   items[index].className="image-gallery-item";
+  //   items[next].className="image-gallery-item-selected";
+  // }
+  //
+  // function showPrev(){
+  //   var carousel = document.getElementsByClassName("image-gallery-items");
+  //   var items = carousel[0].children;
+  //   var length = items.length;
+  //   var selected = document.getElementsByClassName("image-gallery-item-selected")[0];
+  //   var index = Array.prototype.indexOf.call(items, selected);
+  //   var prev = index == 0 ? length - 1 : index - 1;
+  //
+  //   console.log(items[index], items[prev]);
+  //
+  //   items[index].className="image-gallery-item";
+  //   items[prev].className="image-gallery-item-selected";
+  // }
+  //
+  // next.onclick=showNext;
+  // prev.onclick=showPrev;
 
-  function showNext(){
-    var carousel = document.getElementsByClassName("image-gallery-items");
-    var items = carousel[0].children;
-    var length = items.length;
-    var selected = document.getElementsByClassName("image-gallery-item-selected")[0];
-    var index = Array.prototype.indexOf.call(items, selected);
-    var next = index >= length - 1 ? 0 : index + 1;
-
-    console.log(items[index], items[next]);
-
-
-    items[index].className="image-gallery-item";
-    items[next].className="image-gallery-item-selected";
-  }
-
-  function showPrev(){
-    var carousel = document.getElementsByClassName("image-gallery-items");
-    var items = carousel[0].children;
-    var length = items.length;
-    var selected = document.getElementsByClassName("image-gallery-item-selected")[0];
-    var index = Array.prototype.indexOf.call(items, selected);
-    var prev = index == 0 ? length - 1 : index - 1;
-
-    console.log(items[index], items[prev]);
-
-    items[index].className="image-gallery-item";
-    items[prev].className="image-gallery-item-selected";
-  }
-
-  next.onclick=showNext;
-  prev.onclick=showPrev;
+  $(document).ready(function(){
+  $('.image-gallery').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    mobileFirst: true
+  });
+});
 
 });
